ad84b0dc3700e30ee4a95da5bc04b59e990e6fa8
Atishay Tibrewal <atishay1309@gmail.com>
2014-08-30 10:17:34 +0530

    Initial commit

---
+++ twitterStream.py
@@ -0,0 +1,6 @@
+api_key = "UPKiERi4MFVHHVN5OPQ6NGdJb"
+api_secret = "kvWN18BzOVy3oL7DM6gz8v0SHddEETCqykGtCF17yqUWRX5Lto"
+access_token_key = "345747358-4UKA1e3gxLhpbxlR6dR7L5Q2PBTysWIk8LtfKvkB"
+access_token_secret = "Z7MtrXCDtP3BAkXLnkqlKqffcjb95twGlVbzrkzAJvgAt"
+
+
